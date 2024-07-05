f0d376f1c3bba1ed8bf32f0c912eaa36c1c68dd9
Gugulator <93097630+Gugulator@users.noreply.github.com>
2024-07-05 14:10:36 +0300

    Update __init__.py
    due to requirements of Home Assistant core 2024.7.0

--- custom_components/boi_exchange_rates/__init__.py
+++ custom_components/boi_exchange_rates/__init__.py
@@ -44,9 +44,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     entry.async_on_unload(entry.add_update_listener(update_listener))
 
     # Setup sensor platform
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+    await hass.config_entries.async_forward_entry_setup(entry, "sensor")
 
     return True
 
